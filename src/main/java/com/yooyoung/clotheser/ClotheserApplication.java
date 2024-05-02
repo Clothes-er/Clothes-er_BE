61aa7e3bd4a73bd9b0cb24077bb4f6ca54fabaeb
ilu25 <acejsy@gmail.com>
2024-05-02 19:16:33 +0900

    chore: Springboot 초기 코드 세팅

---
+++ src/main/java/com/yooyoung/clotheser/ClotheserApplication.java
@@ -0,0 +1,13 @@
+package com.yooyoung.clotheser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClotheserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClotheserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yooyoung/clotheser/ClotheserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yooyoung.clotheser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClotheserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
