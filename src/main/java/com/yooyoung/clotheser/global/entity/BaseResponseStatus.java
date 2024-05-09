d23e2a8fbd7f938f3460154a9f3d1de36f4a0379
ilu25 <acejsy@gmail.com>
2024-05-10 00:22:03 +0900

    feat: 기본 응답 및 에러 설정

---
+++ src/main/java/com/yooyoung/clotheser/global/entity/BaseResponseStatus.java
@@ -0,0 +1,58 @@
+package com.yooyoung.clotheser.global.entity;
+
+import lombok.Getter;
+
+@Getter
+public enum BaseResponseStatus {
+
+    /*
+        1000 : 요청 성공
+    */
+    SUCCESS(true, 1000, "요청에 성공하였습니다."),
+
+
+    /*
+        2000 : Request 오류
+    */
+    // 1. Common (2000 ~ 2099)
+    REQUEST_ERROR(false, 2000, "입력값을 확인해주세요."),
+    EMPTY_JWT(false, 2001, "JWT 토큰을 입력해주세요."),
+    INVALID_USER_JWT(false, 2002, "권한이 없는 유저의 접근입니다."),
+    INVALID_JWT_TOKEN(false, 2003, "유효하지 않은 JWT 토큰입니다."),
+    EXPIRED_JWT_TOKEN(false, 2004, "만료된 JWT 토큰입니다."),
+    UNSUPPORTED_JWT_TOKEN(false, 2005, "지원하지 않는 JWT 토큰입니다."),
+    EMPTY_JWT_CLAIMS(false, 2006, "JWT 토큰이 잘못되었습니다."),
+
+    // 2. User (2100 ~ 2199)
+    // 회원가입
+    PASSWORD_CONFIRMATION_MISMATCH(false, 2100, "비밀번호가 일치하지 않습니다."),
+    NICKNAME_EXISTS(false, 2101, "중복된 닉네임입니다."),
+    EMAIL_EXISTS(false, 2102, "중복된 이메일입니다."),
+    PHONE_NUMBER_EXISTS(false, 2103, "중복된 전화번호입니다."),
+
+
+    /*
+        3000 : Response 오류
+    */
+    // Common
+    RESPONSE_ERROR(false, 3000, "값을 불러오는데 실패하였습니다."),
+
+
+    /*
+        4000 : Database, Server 오류
+    */
+    // Common
+    DATABASE_ERROR(false, 4000, "데이터베이스 연결에 실패하였습니다."),
+    SERVER_ERROR(false, 4001, "서버와의 연결에 실패하였습니다.");
+
+
+    private final boolean isSuccess;
+    private final int code;
+    private final String message;
+
+    private BaseResponseStatus(boolean isSuccess, int code, String message) {
+        this.isSuccess = isSuccess;
+        this.code = code;
+        this.message = message;
+    }
+}
