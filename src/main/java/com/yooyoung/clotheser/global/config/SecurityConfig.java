7e59af0300f3560d9763ea4e89237a2b58a37061
ilu25 <acejsy@gmail.com>
2024-05-12 23:29:51 +0900

    style: 불필요한 코드 제거

--- src/main/java/com/yooyoung/clotheser/global/config/SecurityConfig.java
+++ src/main/java/com/yooyoung/clotheser/global/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.yooyoung.clotheser.global.config;
 
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
