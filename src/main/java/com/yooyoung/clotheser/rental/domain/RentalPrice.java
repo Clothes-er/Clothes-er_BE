ab4a2736cc2b6d38217900b060cb4edd4ed434c8
ilu25 <acejsy@gmail.com>
2024-05-19 19:24:44 +0900

    feat: 공유 옷장 관련 엔티티 생성

---
+++ src/main/java/com/yooyoung/clotheser/rental/domain/RentalPrice.java
@@ -0,0 +1,30 @@
+package com.yooyoung.clotheser.rental.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.DynamicUpdate;
+
+@Getter
+@Builder
+@Entity
+@DynamicUpdate
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class RentalPrice {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true, nullable = false)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(nullable = false)
+    private Rental rental;
+
+    @Column(nullable = false)
+    private int price;
+
+    @Column(nullable = false)
+    private int days;
+
+}
