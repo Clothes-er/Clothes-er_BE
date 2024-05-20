8538b9c926b42f45db6a53c4ba02e3fab3e09685
ilu25 <acejsy@gmail.com>
2024-05-20 21:55:21 +0900

    fix: 대여글 생성 API try-catch 제거 - 차후 예외 처리 추가

--- src/main/java/com/yooyoung/clotheser/rental/controller/RentalController.java
+++ src/main/java/com/yooyoung/clotheser/rental/controller/RentalController.java
@@ -33,25 +33,20 @@ public class RentalController {
                                                                          BindingResult bindingResult,
                                                                          @PathVariable Long clothesId,
                                                                          @AuthenticationPrincipal CustomUserDetails userDetails) {
-        try {
-            // 입력 유효성 검사
-            if (bindingResult.hasErrors()) {
-                List<FieldError> list = bindingResult.getFieldErrors();
-                for(FieldError error : list) {
-                    return new ResponseEntity<>(new BaseResponse<>(REQUEST_ERROR, error.getDefaultMessage()), BAD_REQUEST);
-                }
+        // 입력 유효성 검사
+        if (bindingResult.hasErrors()) {
+            List<FieldError> list = bindingResult.getFieldErrors();
+            for(FieldError error : list) {
+                return new ResponseEntity<>(new BaseResponse<>(REQUEST_ERROR, error.getDefaultMessage()), BAD_REQUEST);
             }
-
-            // 가격 입력 확인
-            if (postRentalRequest.getPrices() == null || postRentalRequest.getPrices().isEmpty()) {
-                return new ResponseEntity<>(new BaseResponse<>(REQUEST_ERROR, "가격을 입력해주세요."), BAD_REQUEST);
-            }
-
-            return new ResponseEntity<>(new BaseResponse<>(rentalService.createRentalPost(postRentalRequest, clothesId, userDetails.user)), CREATED);
         }
-        catch (BaseException exception) {
-            return new ResponseEntity<>(new BaseResponse<>(exception.getStatus()), exception.getHttpStatus());
+
+        // 가격 입력 확인
+        if (postRentalRequest.getPrices() == null || postRentalRequest.getPrices().isEmpty()) {
+            return new ResponseEntity<>(new BaseResponse<>(REQUEST_ERROR, "가격을 입력해주세요."), BAD_REQUEST);
         }
+
+        return new ResponseEntity<>(new BaseResponse<>(rentalService.createRentalPost(postRentalRequest, clothesId, userDetails.user)), CREATED);
     }
 
 }
