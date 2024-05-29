31ce7b871c194fe4935b1413717b758896ffce87
ilu25 <acejsy@gmail.com>
2024-05-29 22:02:36 +0900

    chore: 웹소켓 환경 설정

---
+++ src/main/java/com/yooyoung/clotheser/global/config/WebSocketConfig.java
@@ -0,0 +1,40 @@
+package com.yooyoung.clotheser.global.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+@RequiredArgsConstructor
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    // 클라이언트가 웹소켓 서버에 연결하는데 사용할 웹소켓 엔드포인트 등록
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+
+        // 웹소켓이 handshake를 하기 위해 연결하는 endpoint
+        registry.addEndpoint("/ws")
+                .setAllowedOriginPatterns("*")
+                // client가 sockjs로 개발되어 있을 때만 필요, client가 java면 필요없음
+                .withSockJS()
+        ;
+    }
+
+    // 한 클라이언트에서 다른 클라이언트로 메시지를 라우팅하는데 사용될 메시지 브로커
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+
+        // sub으로 시작되는 요청을 구독한 모든 사용자들에게 메시지를 broadcast
+        // 서버 -> 클라이언트로 발행하는 메세지에 대한 endpoint 설정 : 구독
+        registry.enableSimpleBroker("/sub");
+
+        // pub로 시작되는 메시지는 message-handling methods로 라우팅 됨
+        // 클라이언트 -> 서버로 발행하는 메세지에 대한 endpoint 설정 : 구독에 대한 메세지
+        registry.setApplicationDestinationPrefixes("/pub");
+    }
+
+}
