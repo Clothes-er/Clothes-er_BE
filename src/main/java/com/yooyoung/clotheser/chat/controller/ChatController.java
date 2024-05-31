8741f500f248737ba76418d1ff40f2a6dba2f684
조세영 <93782168+ilu25@users.noreply.github.com>
2024-05-31 21:29:09 +0900

    feat: 채팅방 생성 API (#33) (#34)

--- src/main/java/com/yooyoung/clotheser/chat/controller/ChatController.java
+++ src/main/java/com/yooyoung/clotheser/chat/controller/ChatController.java
@@ -1,15 +1,35 @@
 package com.yooyoung.clotheser.chat.controller;
 
+import com.yooyoung.clotheser.chat.dto.ChatRoomResponse;
+import com.yooyoung.clotheser.chat.service.ChatService;
+import com.yooyoung.clotheser.global.entity.BaseException;
+import com.yooyoung.clotheser.global.entity.BaseResponse;
+import com.yooyoung.clotheser.user.domain.CustomUserDetails;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.http.HttpStatus.CREATED;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/chats")
+@RequestMapping("/api/v1/chat")
 public class ChatController {
 
+    private final ChatService chatService;
+
     // 채팅방 생성
+    @PostMapping("/rooms/{rentalId}")
+    public ResponseEntity<BaseResponse<ChatRoomResponse>> createChatRoom(@PathVariable("rentalId") Long rentalId,
+                                                                         @AuthenticationPrincipal CustomUserDetails userDetails) {
+        try {
+            return new ResponseEntity<>(new BaseResponse<>(chatService.createChatRoom(rentalId, userDetails.user)), CREATED);
+        }
+        catch (BaseException exception) {
+            return new ResponseEntity<>(new BaseResponse<>(exception.getStatus()), exception.getHttpStatus());
+        }
+    }
 
     // 채팅방 목록 조회
 
--- src/main/java/com/yooyoung/clotheser/chat/dto/ChatRoomResponse.java
+++ src/main/java/com/yooyoung/clotheser/chat/dto/ChatRoomResponse.java
@@ -1,6 +1,9 @@
 package com.yooyoung.clotheser.chat.dto;
 
+import com.yooyoung.clotheser.chat.domain.ChatRoom;
+import com.yooyoung.clotheser.rental.domain.Rental;
 import com.yooyoung.clotheser.rental.domain.RentalState;
+
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.Setter;
@@ -13,12 +16,14 @@ import java.util.List;
 public class ChatRoomResponse {
 
     private Long id;
-    private Long buyer;
-    private Long lender;
+    private Long buyerId;
+    private Long lenderId;
+
+    private String opponentNickname;
 
     // 대여글 정보
     private Long rentalId;
-    private String imgUrl;
+    private String rentalImgUrl;
     private String title;
     private int minPrice;
 
@@ -26,5 +31,19 @@ public class ChatRoomResponse {
 
     // 채팅 메시지 목록
     private List<ChatMessageResponse> messages;
+
+    public ChatRoomResponse(ChatRoom chatRoom, String opponentNickname,
+                            Rental rental, String rentalImgUrl, int minPrice) {
+        this.id = chatRoom.getId();
+        this.buyerId = chatRoom.getBuyer().getId();
+        this.lenderId = chatRoom.getLender().getId();
+
+        this.opponentNickname = opponentNickname;
+
+        this.rentalId = rental.getId();
+        this.rentalImgUrl = rentalImgUrl;
+        this.title = rental.getTitle();
+        this.minPrice = minPrice;
+    }
 
 }
