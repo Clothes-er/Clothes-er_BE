58ebbdb02b36fe8b817507bb219fd535857a7e2f
ilu25 <acejsy@gmail.com>
2024-06-28 00:25:54 +0900

    style: 주소 및 프로필 사진 변경 응답값과 개인정보 조회 응답값에 updatedAt 추가 - 주소와 프로필 사진이 변경될 때 회원의 updatedAt도 변경

--- src/main/java/com/yooyoung/clotheser/user/dto/AddressResponse.java
+++ src/main/java/com/yooyoung/clotheser/user/dto/AddressResponse.java
@@ -1,8 +1,11 @@
 package com.yooyoung.clotheser.user.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.yooyoung.clotheser.user.domain.User;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
+
+import java.time.LocalDateTime;
 
 @Data
 @Setter(AccessLevel.NONE)
@@ -17,10 +20,15 @@ public class AddressResponse {
     @Schema(title = "경도", example = "127.026905")
     private double longitude;
 
+    @Schema(title = "회원 정보 수정한 시간", example = "2024년 06월 20일 19:13:36")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy년 MM월 dd일 HH:mm:ss", timezone = "Asia/Seoul")
+    private LocalDateTime updatedAt;
+
     public AddressResponse(User user) {
         this.email = user.getEmail();
         this.latitude = user.getLatitude();
         this.longitude = user.getLongitude();
+        this.updatedAt = user.getUpdatedAt();
     }
 
 }
