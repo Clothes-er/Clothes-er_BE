142ff08767c400ad8b1460ad5966664076740e8f
ilu25 <acejsy@gmail.com>
2024-07-02 00:39:29 +0900

    style: 불필요한 import문 삭제

--- src/main/java/com/yooyoung/clotheser/global/config/SecurityConfig.java
+++ src/main/java/com/yooyoung/clotheser/global/config/SecurityConfig.java
@@ -19,11 +19,8 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
 
 import java.io.PrintWriter;
-import java.util.List;
 
 import static com.yooyoung.clotheser.global.entity.BaseResponseStatus.*;
 import static org.springframework.http.HttpStatus.*;
