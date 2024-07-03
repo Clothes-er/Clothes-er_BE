61697864d6e207e345e5233f6e3a28e2a977e3e7
ilu25 <acejsy@gmail.com>
2024-07-03 14:54:36 +0900

    fix: Swagger CORS 에러 해결 - 배포 서버에서 Swagger 접속 시 http로 되어서 URL 설정

--- src/main/java/com/yooyoung/clotheser/global/config/SwaggerConfig.java
+++ src/main/java/com/yooyoung/clotheser/global/config/SwaggerConfig.java
@@ -6,11 +6,18 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
+
 @Configuration
 public class SwaggerConfig {
+
+    @Value("${swagger.server-url}")
+    private String serverUrl;
 
     @Bean
     public OpenAPI openAPI() {
@@ -25,7 +32,8 @@ public class SwaggerConfig {
         return new OpenAPI()
                 .components(new Components().addSecuritySchemes("Bearer Token", apiKey))
                 .addSecurityItem(securityRequirement)
-                .info(apiInfo());
+                .info(apiInfo())
+                .servers(List.of(new Server().url(serverUrl)));  // 환경에 따라 서버 URL 설정
     }
 
     private Info apiInfo() {
