41629e0cad9bc92ccc7b4f9c98de4fe6c6cc57f0
ilu25 <acejsy@gmail.com>
2024-08-01 16:48:09 +0900

    chore: 보유 옷 관련 엔티티 생성

---
+++ src/main/java/com/yooyoung/clotheser/clothes/domain/Clothes.java
@@ -0,0 +1,72 @@
+package com.yooyoung.clotheser.clothes.domain;
+
+import com.yooyoung.clotheser.user.domain.Gender;
+import com.yooyoung.clotheser.user.domain.User;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.DynamicUpdate;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
+
+@Builder
+@Getter
+@DynamicUpdate
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+public class Clothes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true, nullable = false)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(nullable = false)
+    private User user;
+
+    @Column(nullable = false, length = 50)
+    private String name;
+
+    @Column(columnDefinition = "TINYINT(1)")
+    private Gender gender;
+
+    @Column(length = 10)
+    private String category;
+
+    private Integer price;
+
+    @Column(length = 20)
+    private String brand;
+
+    @Column(length = 10)
+    private String size;
+
+    private String shoppingUrl;
+
+    @Column(nullable = false, length = 500)
+    private String description;
+
+    @Column(columnDefinition = "TINYINT(1)", nullable = false)
+    @ColumnDefault("true")
+    private Boolean isPublic;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @ColumnDefault("null")
+    private LocalDateTime updatedAt;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @ColumnDefault("null")
+    private LocalDateTime deletedAt;
+
+}
---
+++ src/main/java/com/yooyoung/clotheser/clothes/domain/ClothesImg.java
@@ -0,0 +1,25 @@
+package com.yooyoung.clotheser.clothes.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Builder
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ClothesImg {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true, nullable = false)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(nullable = false)
+    private Clothes clothes;
+
+    @Column(nullable = false)
+    private String imgUrl;
+
+}
