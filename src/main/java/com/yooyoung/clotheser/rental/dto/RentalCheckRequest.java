89bc1614f680ebd4120068c25328a4f37e643b67
ilu25 <acejsy@gmail.com>
2024-08-03 12:12:38 +0900

    fix: List 타입 @NotBlank -> @NotEmpty 변경

--- src/main/java/com/yooyoung/clotheser/rental/dto/RentalCheckRequest.java
+++ src/main/java/com/yooyoung/clotheser/rental/dto/RentalCheckRequest.java
@@ -2,7 +2,7 @@ package com.yooyoung.clotheser.rental.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.*;
 
 import java.util.List;
@@ -14,7 +14,7 @@ public class RentalCheckRequest {
     @Schema(title = "옷 상태 체크리스트", description = "255자 이내", type = "array",
             example = "[\"오른쪽 소매에 오염 있음\", \"구김 있음\"]", requiredMode = Schema.RequiredMode.REQUIRED)
     @Valid
-    @NotBlank(message = "옷 상태를 입력해주세요.")
+    @NotEmpty(message = "옷 상태를 입력해주세요.")
     private List<String> checkList;
 
 }
