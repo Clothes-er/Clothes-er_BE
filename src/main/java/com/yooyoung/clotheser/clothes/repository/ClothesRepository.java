c7bbbf67dd68d0ab2de821c0a2714d119871da26
ilu25 <acejsy@gmail.com>
2024-10-15 19:29:09 +0900

    feat: 나의 보유 옷 찜 목록 조회 API (#120)

--- src/main/java/com/yooyoung/clotheser/clothes/repository/ClothesRepository.java
+++ src/main/java/com/yooyoung/clotheser/clothes/repository/ClothesRepository.java
@@ -2,6 +2,7 @@ package com.yooyoung.clotheser.clothes.repository;
 
 import com.yooyoung.clotheser.clothes.domain.Clothes;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -22,4 +23,10 @@ public interface ClothesRepository extends JpaRepository<Clothes, Long> {
     // 대여글 작성 - 대여글 없는 보유 옷 목록 조회
     List<Clothes> findAllByUserIdAndRentalIdNullAndDeletedAtNullOrderByCreatedAtDesc(Long userId);
 
+    // 찜한 보유 옷 목록 조회
+    @Query("select c from Clothes c " +
+            "join ClothesLike cl on c.id = cl.clothes.id " +
+            "where cl.user.id = :userId and c.deletedAt is null and cl.deletedAt is null " +
+            "order by cl.createdAt desc")
+    List<Clothes> findLikeClothes(Long userId);
 }
