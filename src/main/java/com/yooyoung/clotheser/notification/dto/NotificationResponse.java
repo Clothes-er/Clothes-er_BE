88e3e728a9d0f1cd71a1728c78acefe27df41339
ilu25 <acejsy@gmail.com>
2024-10-29 23:23:47 +0900

    style: 알림 목록 응답값에 종류, 출처 id 추가 - 알림 조회 시 읽음 처리만 하고 응답값은 따로 주지 않도록 목록에서 보여주기

--- src/main/java/com/yooyoung/clotheser/notification/dto/NotificationResponse.java
+++ src/main/java/com/yooyoung/clotheser/notification/dto/NotificationResponse.java
@@ -1,5 +1,6 @@
 package com.yooyoung.clotheser.notification.dto;
 
+import com.yooyoung.clotheser.notification.domain.NotificationType;
 import com.yooyoung.clotheser.notification.domain.PushNotification;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AccessLevel;
@@ -21,6 +22,12 @@ public class NotificationResponse {
     @Schema(title = "알림 내용", example = "신고 내용 검토 결과 신고가 반려되었습니다.")
     private String content;
 
+    @Schema(title = "알림 종류")
+    private NotificationType type;
+
+    @Schema(title = "알림 출처 id", example = "1", description = "null / 팔로워 id")
+    private Long sourceId;
+
     @Schema(title = "알림 확인 여부", example = "false")
     private Boolean isRead;
 
@@ -29,6 +36,8 @@ public class NotificationResponse {
         this.image = image;
         this.title = notification.getTitle();
         this.content = notification.getContent();
+        this.type = notification.getType();
+        this.sourceId = notification.getSourceId();
         this.isRead = notification.getIsRead();
     }
 }
